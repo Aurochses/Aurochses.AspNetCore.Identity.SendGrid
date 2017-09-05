99af4989f7f9cb8ad7ca1ff46196be95337ea8f5
Vladimir Rodchenko <vladimir@aurochses.com>
2017-09-05 12:42:16 +0300

    added code and tests

---
+++ src/Aurochses.Identity.SendGrid/SendGridOptions.cs
@@ -0,0 +1,48 @@
+﻿namespace Aurochses.Identity.SendGrid
+{
+    /// <summary>
+    /// Class SendGridOptions.
+    /// </summary>
+    public class SendGridOptions
+    {
+        /// <summary>
+        /// Gets or sets from email address.
+        /// </summary>
+        /// <value>
+        /// From email address.
+        /// </value>
+        public string FromEmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of from email.
+        /// </summary>
+        /// <value>
+        /// The name of from email.
+        /// </value>
+        public string FromEmailName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email confirmation token template identifier.
+        /// </summary>
+        /// <value>
+        /// The email confirmation token template identifier.
+        /// </value>
+        public string EmailConfirmationTokenTemplateId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password reset token template identifier.
+        /// </summary>
+        /// <value>
+        /// The password reset token template identifier.
+        /// </value>
+        public string PasswordResetTokenTemplateId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the two factor token template identifier.
+        /// </summary>
+        /// <value>
+        /// The two factor token template identifier.
+        /// </value>
+        public string TwoFactorTokenTemplateId { get; set; }
+    }
+}
